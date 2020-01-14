20d0e19a4aebaf224f6ec2c1d6abbecfede03095
Jacqueline Freeland <secretartist272@gmail.com>
2020-01-14 18:10:29 +0000

    first commit

---
+++ lib/cli_dinosaur.rb
@@ -0,0 +1,6 @@
+require "cli_dinosaur/version"
+
+module CliDinosaur
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/cli_dinosaur/version.rb
@@ -0,0 +1,3 @@
+module CliDinosaur
+  VERSION = "0.1.0"
+end
